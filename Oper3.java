fb95786cf092f4662be612e23f2bf617d05cb106
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-11 23:32:14 +0800

    Create Oper3.java

---
+++ Oper3.java
@@ -0,0 +1,22 @@
+/*NIM   : 13020210012
+Nama    : Indah Nirwana
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 14:25 PM*/
+package tugas14;
+
+public class Oper3 {
+    
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Algoritma */
+        if (true && true){ System.out.println(true && true); }      /* true = true and true */
+        if (true & true) { System.out.println(true & false); }      /* true & true */
+        if (true) { System.out.println(true); }                     /* true */
+        if (true || true){ System.out.println(true); }              /* true = true or true */
+        if (true|false) { System.out.println(true|false); }         /* true|false */
+        
+    }
+}
