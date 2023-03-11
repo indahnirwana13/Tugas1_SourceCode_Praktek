806c2d797490e6bbe8479d421521a8f74ee153d1
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-11 21:34:32 +0800

    Create Asgdll.java

---
+++ Asgdll.java
@@ -0,0 +1,24 @@
+/*NIM   : 13020210012
+Nama    : Indah Nirwana
+Hari/Tgl: Jumat, 10 Maret 2022
+Waktu   : 13:15 PM*/
+package tugas1;
+
+public class Asgdll {
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        float f=20.0f;
+        double fll;
+
+        /* Algoritma */
+        fll= 10.0f;
+        System.out.println("f : " +f+ "\nfll: " +fll);
+    }
+    
+}
