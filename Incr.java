1779ddaa85da8f1dcf2fbb2dce569cd070c41007
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-11 23:27:12 +0800

    Create Incr.java

---
+++ Incr.java
@@ -0,0 +1,25 @@
+/*NIM   : 13020210012
+Nama    : Indah Nirwana
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 14:05 PM*/
+package tugas11;
+
+/* Effek dari operator ++ */
+public class Incr {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        int i, j;
+
+        /* Program */
+        i = 3;
+        j = i++;
+
+        System.out.println("Nilai i : " + (++i) + "\nNilai j : " + j);
+    }
+    
+}
