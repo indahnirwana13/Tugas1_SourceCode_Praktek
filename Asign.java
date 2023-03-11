56b73837a144007d4c6aac60bc56a7ba7d96bbc0
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-11 23:08:14 +0800

    Create Asign.java

---
+++ Asign.java
@@ -0,0 +1,23 @@
+/*NIM   : 13020210012
+Nama    : Indah Nirwana
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 13:20 AM*/
+package tugas2;
+
+public class Asign {
+
+    /**
+     * @param args
+     */
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* kamus */
+        int i;
+        /* Program */
+        System.out.print("hello\n"); i=5;
+        System.out.println("Ini nilai i : " +i);
+    }
+    
+}
