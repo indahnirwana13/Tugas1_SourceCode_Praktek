6766cf679af7b5a4c7a3118fbbfad0708644ea61
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-11 23:12:16 +0800

    Create BacaData.java

---
+++ BacaData.java
@@ -0,0 +1,26 @@
+/*NIM   : 13020210012
+Nama    : Indah Nirwana
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 13:30 AM*/
+package tugas4;
+import java.util.Scanner;
+
+public class BacaData {
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        //TODO Auto-generated method stub
+        /* Kamus */
+        int a;
+        Scanner masukan;
+        /* Program */
+        System.out.print("Contoh membaca dan menulis, ketik nilai integer : \n");
+        masukan = new Scanner(System.in);
+        a = masukan.nextInt(); /* coba ketik : masukan.nextInt(); Apa akibatnya?*/
+        System.out.print("Nilai yang dibaca : " +a);
+    }
+    
+}
