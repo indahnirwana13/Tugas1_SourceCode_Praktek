0400d3ce9dfd3301f12544fa1be393627a3d59e7
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-11 23:22:29 +0800

    Create Ekspresi.java

---
+++ Ekspresi.java
@@ -0,0 +1,24 @@
+/*NIM   : 13020210012
+Nama    : Indah Nirwana
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 13:50 PM*/
+package tugas8;
+
+/* Pemakaian operator kondisional */
+public class Ekspresi {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* KAMUS */
+        int x = 1;
+        int y = 2;
+        /* ALGORITMA */
+        System.out.print("x = "+ x + "\n");
+        System.out.print("y = "+ y + "\n");
+        System.out.print("hasil ekspresi = (x<y)?x:y = "+ ((x < y) ? x : y)); 
+        /*Gunakan dalam kurung "(statement dan kondisi)" untuk menyatakan satu kesatuan pernyataan*/
+    }
+}
